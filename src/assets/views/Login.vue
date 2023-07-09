71bc2b2468cb7bd4eddda4a30b59f94ca5341ca0
locazy <95532378+locazy@users.noreply.github.com>
2023-07-09 15:55:42 +0800

    Add files via upload

---
+++ src/assets/views/Login.vue
@@ -0,0 +1,231 @@
+<template>
+  <div class="background"></div>
+
+  <div class="form-box">
+    <el-form ref="ruleFormRef" :model="form" :rules="rules" status-icon>
+      <!-- logo -->
+      <div style="width: 400px;display: flex; padding-left: 19%">
+        <img src="../assets/imgs/logo/logo1.png" alt="" style="width: 30px; margin-bottom: 10px;margin-right: 8px">
+<!--        <div style="text-align: center; font-weight: bold; color: white; font-size: x-large; padding-bottom: 10px">-->
+<!--          系统-->
+<!--        </div>-->
+      </div>
+      <el-form-item prop="username">
+        <el-input v-model="form.username" placeholder="请输入账号或邮箱" :prefix-icon="User"></el-input>
+      </el-form-item>
+      <el-form-item prop="password">
+        <el-input v-model="form.password" show-password placeholder="请输入密码" :prefix-icon="Lock"
+                  autocomplete="new-password"></el-input>
+      </el-form-item>
+      <div style="margin-bottom: 0.83em">
+        <el-button style="width: 100%; background-color: dodgerblue" type="primary" @click="login">登 录</el-button>
+      </div>
+      <div style="text-align: right">
+        <el-button link style="float: left; color: #aaa" @click="handleResetPassword">忘记密码</el-button>
+        <el-button type="primary" link @click="router.push('/register')">没有注册账号？前往注册</el-button>
+      </div>
+    </el-form>
+  </div>
+
+  <el-dialog v-model="passwordVis" title="忘记密码" :close-on-click-modal="false" style="width: 500px; padding: 0 20px">
+    <el-form :model="passwordForm" ref="rulePasswordFormRef" :rules="passwordRules" status-icon label-width="70px">
+      <el-form-item label="用户名" prop="userName">
+        <el-input v-model="passwordForm.userName" autocomplete="off"/>
+      </el-form-item>
+      <el-form-item label="新密码" prop="password">
+        <el-input v-model="passwordForm.password" autocomplete="off"/>
+      </el-form-item>
+      <el-form-item label="验证码" prop="verifyCode">
+        <div style="display: flex; width: 100%">
+          <el-input style="flex: 1" v-model="passwordForm.verifyCode" clearable></el-input>
+          <el-button style="width: 120px; margin-left: 5px" @click="sendEmail" :disabled="time > 0">点击发送<span
+              v-if="time">({{ time }})</span></el-button>
+        </div>
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="passwordVis=false">取消</el-button>
+        <el-button type="primary" @click="resetPassword">确认</el-button>
+      </span>
+    </template>
+  </el-dialog>
+</template>
+
+<script setup>
+import {nextTick, reactive, ref} from "vue";
+import {User, Lock} from '@element-plus/icons-vue'
+import router from "@/router";
+import request from "@/utils/request";
+import {ElMessage} from "element-plus";
+import {inject} from "vue";
+import {useStore} from "vuex";
+import axios from 'axios';
+import store from "@/stores/store";
+
+const reload = inject('reload') // inject和父页面的provide成对出现(App.vue)
+const ruleFormRef = ref()
+const rulePasswordFormRef = ref()
+const passwordVis = ref(false)
+const interval = ref(-1)
+const time = ref(0)
+
+const rules = reactive({
+  username: [
+    {required: true, message: '请输入账号', trigger: 'blur'},
+    {min: 4, max: 16, message: '长度需在4~16之间', trigger: 'blur'},
+  ],
+  password: [
+    {required: true, message: '请输入密码', trigger: 'blur'},
+    {min: 4, max: 24, message: '长度需在4~24之间', trigger: 'blur'},
+  ],
+})
+
+const passwordRules = reactive({
+  userName: [
+    {required: true, message: '请输入用户名', trigger: 'blur'},
+  ],
+  verifyCode: [
+    {required: true, message: '请输入验证码', trigger: 'blur'},
+  ],
+  password: [
+    {required: true, message: '请输入新的密码', trigger: 'blur'},
+  ],
+})
+
+const form = reactive({})
+const passwordForm = reactive({})
+const userStore = useStore()
+
+
+const login = () => {
+  let data = {
+    userName: form.username,
+    password: form.password
+  }
+  data = JSON.stringify(data)
+  console.log(ruleFormRef)
+  ruleFormRef.value.validate(valid => {
+    // 当valid == true 就可以调用接口了
+    if (valid) {
+      request.post("http://123.249.101.81:8080/users/login", data).then(res => {
+        userStore.commit('setUserData', res);
+        console.log(userStore.state.userData.obj);
+        if (res.code == '200') {
+          // store.$patch({user: res.data}) // res.data 是后台返回的用户数据，存储到缓存里
+          ElMessage.success('登录成功')
+          localStorage.setItem('token',res.obj.token)
+          console.log(localStorage)
+          if (res.obj.role === "user") {
+            router.push('/home')
+            reload()
+          } else if (res.obj.role === "admin") {
+            router.push('/management/welcome')
+            reload()
+          }
+        } else {
+          ElMessage.error(res.message)
+        }
+      })
+    }
+  })
+}
+
+const times = () => {
+  // 清空定时器
+  if (interval.value >= 0) {
+    clearInterval(interval.value)
+  }
+  time.value = 60
+  interval.value = setInterval(() => {
+    if (time.value > 0) {
+      time.value--
+    }
+  }, 1000)
+}
+
+// 发送邮件
+const sendEmail = () => {
+  // const reg = /^\w+((.\w+)|(-\w+))@[A-Za-z0-9]+((.|-)[A-Za-z0-9]+).[A-Za-z0-9]+$/
+  // if (!reg.test(passwordForm.userName)) {  // test可以校验你的输入值
+  //   ElMessage.warning("请输入合法的邮箱")
+  //   return
+  // }
+  const reg = /^\w{4,16}$/
+  if (!reg.test(form.username)) {
+    console.log("请输入4-16位的用户名")
+    return
+  }
+  let data ={
+    userName: passwordForm.userName
+  }
+  request.get("http://123.249.101.81:8080/users/verify",data).then(res => {
+    if (res.code === '200') {
+      times()  // 倒计时
+      ElMessage.success('发送成功，有效期5分钟')
+    } else {
+      ElMessage.error(res.message)
+    }
+  })
+}
+
+// 点击忘记密码触发
+const handleResetPassword = () => {
+  passwordVis.value = true
+  // 触发表单重置
+  nextTick(() => {   // 下个时钟触发
+    rulePasswordFormRef.value.resetFields()
+  })
+}
+
+// 调用新接口重置密码
+const resetPassword = () => {
+  rulePasswordFormRef.value.validate(valid => {
+    if (valid) {
+      request.put("http://123.249.101.81:8080/users/changepsw", passwordForm).then(res => {
+        if (res.code === '200') {
+          ElMessage.success(res.msg)
+          passwordVis.value = false
+        } else {
+          ElMessage.error(res.msg)
+        }
+      })
+    }
+  })
+}
+
+</script>
+
+<style scoped>
+.form-box {
+  width: 400px;
+  border-radius: 10px;
+  margin: 0 auto;
+  box-shadow: 0 0 10px -2px rgba(0, 0, 0, .5);
+  background: rgba(255, 255, 255, 0.1);
+  padding: 50px;
+  z-index: 1;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateX(-50%) translateY(-50%);
+}
+
+
+.background {
+  width: 100%;
+  height: 100%; /**宽高100%是为了图片铺满屏幕 */
+  z-index: -1;
+  position: absolute;
+  /*background-image: url("public/background1.jpg");*/
+  background-color: whitesmoke;
+  background-size: 100% 100%;
+  background-attachment: fixed;
+
+  min-width: 900px;
+
+  justify-content: center;
+  align-items: center;
+}
+
+</style>
---
+++ src/assets/views/Register.vue
@@ -0,0 +1,143 @@
+<template>
+  <div class="background"></div>
+
+  <div class="form-box">
+    <el-form ref="ruleFormRef" :model="form" :rules="rules" status-icon>
+      <h2 style="text-align: center; color: white">注 册</h2>
+      <el-form-item prop="username">
+        <el-input v-model="form.userName" placeholder="请输入账号" :prefix-icon="User"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input v-model="form.nickName" placeholder="请设置昵称" :prefix-icon="EditPen"></el-input>
+      </el-form-item>
+      <el-form-item prop="password">
+        <el-input v-model="form.password1" show-password placeholder="请输入密码" :prefix-icon="Lock"
+                  autocomplete="new-password"></el-input>
+      </el-form-item>
+      <el-form-item prop="confirm">
+        <el-input v-model="form.password2" show-password placeholder="请确认密码" :prefix-icon="Lock"
+                  autocomplete="new-password"></el-input>
+      </el-form-item>
+      <el-form-item prop="email">
+        <el-input v-model="form.email" placeholder="请输入邮箱" :prefix-icon="Message"></el-input>
+      </el-form-item>
+      <div style="margin-bottom: 0.83em">
+        <el-button style="width: 100%" type="primary" @click="register">注 册</el-button>
+      </div>
+      <div style="text-align: right">
+        <el-button type="primary" link @click="router.push('/login')">已有账号？前往登录</el-button>
+      </div>
+    </el-form>
+  </div>
+</template>
+
+<script setup>
+import {reactive, ref} from "vue";
+import {User, Lock, EditPen, Message} from '@element-plus/icons-vue'
+import router from "@/router";
+import request from "@/utils/request";
+import {ElMessage} from "element-plus";
+import {inject} from "vue";
+
+const reload = inject('reload') // inject和父页面的provide成对出现(App.vue)
+const form = reactive({})
+const ruleFormRef = ref()
+const time = ref(0)
+const interval = ref(-1)
+
+const confirmPassword = (rule, value, callback) => {
+  if (value == '') {
+    callback(new Error('请确认密码'))
+  }
+  if (form.password != value) {
+    callback(new Error('两次输入密码不一致'))
+  }
+  callback()
+}
+
+const reg = /^\w+((.\w+)|(-\w+))@[A-Za-z0-9]+((.|-)[A-Za-z0-9]+).[A-Za-z0-9]+$/
+const checkEmail = (rule, value, callback) => {
+  if (!reg.test(value)) {  // test可以校验你的输入值
+    return callback(new Error('邮箱格式不合法'));
+  }
+  callback()
+}
+
+const rules = reactive({
+  userName: [
+    {required: true, message: '请输入账号', trigger: 'blur'},
+    {min: 6, max: 16, message: '长度需在6~16之间', trigger: 'blur'},
+  ],
+  nickName: [
+    {min:2,max: 16, message: '昵称长度需在2~16之间', trigger: 'blur'}
+  ],
+  password1: [
+    {required: true, message: '请输入密码', trigger: 'blur'},
+    {min: 6, max: 24, message: '长度需在6~24之间', trigger: 'blur'},
+  ],
+  password2: [
+    {required: true, message: '请确认密码', trigger: 'blur'},
+    {validator: confirmPassword, trigger: 'blur'},
+    {min: 6, max: 24, message: '长度需在6~24之间', trigger: 'blur'},
+  ],
+  email: [
+    {required: true, message: '请验证邮箱', trigger: 'blur'},
+    {validator: checkEmail, trigger: 'blur'},
+  ],
+  // emailCode: [
+  //   {required: true, message: '请输入验证码', trigger: 'blur'},
+  // ]
+})
+
+const register = () => {
+  ruleFormRef.value.validate(valid => {
+    // 当valid == true 就可以调用接口了
+    if (valid) {
+      request.post("http://123.249.101.81:8080/users/register", form).then(res => {
+        console.log(res)
+        console.log(res.message)
+        if (res.code == '200') {
+          ElMessage.success('注册成功，请前往邮箱激活账户')
+          router.push('/login')
+          reload()
+        } else {
+          ElMessage.error(res.message)
+        }
+      })
+    }
+  })
+}
+
+</script>
+
+<style scoped>
+.form-box {
+  width: 400px;
+  border-radius: 10px;
+  margin: 0 auto;
+  box-shadow: 0 0 10px -2px rgba(0, 0, 0, .5);
+  background: rgba(255, 255, 255, 0.1);
+  padding: 50px;
+  z-index: 1;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateX(-50%) translateY(-50%);
+}
+
+.background {
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  position: absolute;
+  background-color: whitesmoke;
+  background-size: 100% 100%;
+  background-attachment: fixed;
+
+  min-width: 900px;
+
+  justify-content: center;
+  align-items: center;
+}
+
+</style>
